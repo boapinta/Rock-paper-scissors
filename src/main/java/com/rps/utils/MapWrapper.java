cab71224928f9d95a816b148759240c045aba5e2
Mariusz Urbanik <boapinta@interia.pl>
2017-07-22 18:48:04 +0200

    Utility class for one-liner map creation

---
+++ src/main/java/com/rps/utils/MapWrapper.java
@@ -0,0 +1,25 @@
+package com.rps.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class MapWrapper<KEY, VALUE> {
+    private Map<KEY, VALUE> map;
+    
+    private MapWrapper() {
+        map = new HashMap<KEY, VALUE>();
+    }
+    
+    public static <KEY, VALUE> MapWrapper<KEY, VALUE> instance() {
+        return new MapWrapper();   
+    }
+    
+    public MapWrapper put(KEY key, VALUE value) {
+        map.put(key, value);
+        return this;
+    }
+    
+    public Map<KEY, VALUE> get() {
+        return map;
+    }
+}
