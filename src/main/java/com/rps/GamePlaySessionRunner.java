b58de15a2a0b404baf9dcf2576750cd70c32fd77
Mariusz Urbanik <boapinta@interia.pl>
2017-07-22 22:11:49 +0200

    Implementing runner for the project

--- src/main/java/com/rps/GamePlaySessionRunner.java
+++ src/main/java/com/rps/GamePlaySessionRunner.java
@@ -2,16 +2,22 @@ package com.rps;
 
 import com.rps.domain.GamePlayOption;
 import com.rps.domain.Player;
+import com.rps.strategy.RandomGamePlayStrategy;
+import com.rps.strategy.FixedGamePlayStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.stream.IntStream;
 
+import static com.rps.domain.GamePlayResult.DRAW;
+import static com.rps.domain.GamePlayResult.WIN;
 import static java.util.stream.Collectors.toList;
 
 public class GamePlaySessionRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(GamePlaySessionRunner.class);
+
+    private static final int DEFAULT_TURNS = 100;
 
     private final Player player1;
     private final Player player2;
@@ -29,7 +35,7 @@ public class GamePlaySessionRunner {
                 .collect(toList()));
     }
 
-    //default modifier to make method visible in test
+    //default modifier to make method visible for test
     GamePlaySessionResult.SingleGamePlay singleGamePlay(Player player1, Player player2) {
         GamePlayOption option1 = player1.play();
         GamePlayOption option2 = player2.play();
@@ -37,5 +43,32 @@ public class GamePlaySessionRunner {
         return GamePlaySessionResult.createSingleGamePlay(
                 GameRules.valueOf(option1.name()).check(option2),
                 GameRules.valueOf(option2.name()).check(option1));
+    }
+
+    public static void main(String... args) {
+        int turns = getTurns(args);
+
+        Player player1 = new Player(new FixedGamePlayStrategy(GamePlayOption.ROCK));
+        Player player2 = new Player(new RandomGamePlayStrategy());
+
+        GamePlaySessionRunner runner = new GamePlaySessionRunner(player1, player2);
+        GamePlaySessionResult result = runner.runSession(turns);
+
+        logger.info("Players played {} times", turns);
+        logger.info("Player1 won {} times", result.countForPlayer1ByResult(WIN));
+        logger.info("Player2 won {} times", result.countForPlayer2ByResult(WIN));
+        logger.info("There were {} draws", result.countForPlayer1ByResult(DRAW));
+    }
+
+    private static int getTurns(String... args) {
+        if (args.length == 1) {
+            try {
+                return Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                logger.warn("NumberFormatException for {}. Using default value {}",
+                        args[0], DEFAULT_TURNS);
+            }
+        }
+        return DEFAULT_TURNS;
     }
 }
